9130828a54373802ce277f031657a86e94990119
Karimovich. <146013797+xasankarimovich@users.noreply.github.com>
2024-04-27 13:52:33 -0700

    gffh

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,8 @@ class MyApp extends StatelessWidget {
         // try changing the seedColor in the colorScheme below to Colors.green
         // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
+        // the co
+        // mmand line to start the app).
         //
         // Notice that the counter didn't reset back to zero; the application
         // state is not lost during the reload. To reset the state, use hot
